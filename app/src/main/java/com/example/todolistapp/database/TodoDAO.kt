d31bd7baa24609087e6ade650f9d11b4993eb462
Ibukun Okunoye <ibkokunoye@gmail.com>
2021-12-03 04:04:01 +0100

    Tododao filled with functions

--- app/src/main/java/com/example/todolistapp/database/TodoDAO.kt
+++ app/src/main/java/com/example/todolistapp/database/TodoDAO.kt
@@ -1,7 +1,27 @@
 package com.example.todolistapp.database
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
 
 class TodoDAO() {
 
+    @Update
+    fun updateTodo(todo: TodoEntity) {}
+
+
+    @Insert
+    fun InsertTodo(todo: TodoEntity) {}
+
+
+
+
+    @Delete
+    fun deleteTodo() {}
+
+    @Query("SELECT * FROM todo_table")
+    fun getAllTodos() {}
 
 }
