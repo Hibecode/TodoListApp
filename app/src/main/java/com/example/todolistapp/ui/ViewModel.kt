06ed7e5ab90ce980efc0aeea3d2df5efe2445356
hibecode <ibkokunoye@gmail.com>
2021-12-10 17:09:50 +0100

    Done with ViewModel.kt

--- app/src/main/java/com/example/todolistapp/ui/ViewModel.kt
+++ app/src/main/java/com/example/todolistapp/ui/ViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.todolistapp.ui
 
-class ViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todolistapp.database.TodoEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.w3c.dom.Entity
+
+class TodoViewModel(private val repo: Repository): ViewModel() {
+
+    fun getAllTodos() = repo.getTodos()
+
+
+    fun deleteTodo(todo: TodoEntity) = viewModelScope.launch(Dispatchers.Main){
+        repo.deleteTodo(todo)
+    }
+
+    fun insertTodo(todo: TodoEntity) = viewModelScope.launch(Dispatchers.Main) {
+        repo.insertTodo(todo)
+    }
+
+    fun updateTodo(todo: TodoEntity) = viewModelScope.launch(Dispatchers.Main) {
+        repo.updateTodo(todo)
+    }
+
+
+
+
 }
