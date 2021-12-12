c6fffaab940b8b1a1f9cbca8b20d13885636cd20
hibecode <ibkokunoye@gmail.com>
2021-12-12 03:55:19 +0100

    Added viewmodel call in mainActivity.kt

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.todolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolistapp.database.TodoEntity
+import com.example.todolistapp.ui.TodoViewModel
+import java.util.EnumSet.of
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewmd = ViewModelProvider(this).get(TodoViewModel::class.java)
 
 
     }
