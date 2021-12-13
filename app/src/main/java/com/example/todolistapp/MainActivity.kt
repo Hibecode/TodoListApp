9bf3c94d810698c9e346f65bf9d13f5dbed29142
hibecode <ibkokunoye@gmail.com>
2021-12-13 13:50:11 +0100

    set up viewmodelfactory and database on main activity

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.todolistapp.database.TodoDatabase
 import com.example.todolistapp.database.TodoEntity
+import com.example.todolistapp.ui.Repository
 import com.example.todolistapp.ui.TodoViewModel
+import com.example.todolistapp.ui.TodoViewModelFactory
 import java.util.EnumSet.of
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewmd = ViewModelProvider(this).get(TodoViewModel::class.java)
+
+        val database = TodoDatabase.getInstance(applicationContext)
+        val repository = Repository(database)
+        val factory = TodoViewModelFactory(repository)
+
+
+        val viewModel = ViewModelProvider(this).get(TodoViewModel::class.java)
 
 
     }
--- app/src/main/java/com/example/todolistapp/ui/TodoViewModelFactory.kt
+++ app/src/main/java/com/example/todolistapp/ui/TodoViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.example.todolistapp.ui
 
-class TodoViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class TodoViewModelFactory(private val repository: Repository): ViewModelProvider.Factory {
+
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return TodoViewModel(repository) as T
+    }
 }
