a5d0bd253fa0477d73b641c86bb0e805566d943b
hibecode <ibkokunoye@gmail.com>
2021-12-14 15:37:20 +0100

    Done with the mydiffutilclass

--- app/src/main/java/com/example/todolistapp/ui/MyDiffUtil.kt
+++ app/src/main/java/com/example/todolistapp/ui/MyDiffUtil.kt
@@ -1,4 +1,32 @@
 package com.example.todolistapp.ui
 
-class MyDiffUtil {
+import androidx.recyclerview.widget.DiffUtil
+import com.example.todolistapp.database.TodoEntity
+
+class MyDiffUtil(
+    private val oldList: List<TodoEntity>,
+    private val newList: List<TodoEntity>
+): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+       return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return  when{
+            oldList[oldItemPosition].id != newList[newItemPosition].id -> false
+            oldList[oldItemPosition].title != newList[newItemPosition].title -> false
+            oldList[oldItemPosition].isChecked != newList[newItemPosition].isChecked -> false
+            else -> true
+        }
+    }
+
+
 }
